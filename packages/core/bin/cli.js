7101159819e0b8264b06f877465ef7ec501de0d6
niubingchuan <niubingchuan@enn.cn>
2022-03-02 14:24:47 +0800

    fix: test import-local

---
+++ packages/core/bin/cli.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+'use strict';
+
+const importLocal = require("import-local")
+console.log(__filename);
+if (importLocal(__filename)) {
+    console.log('run local');
+}else {
+    require('.')
+}
--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,8 +1,2 @@
-#!/usr/bin/env node
-'use strict';
-
-const lib = require("bc-cli-l")
 const add = require("@bc-cli/utils")
-
-lib.logfilename()
 console.log(add(100,200));
